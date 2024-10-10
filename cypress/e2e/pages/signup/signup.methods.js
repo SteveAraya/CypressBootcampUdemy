d7cbfce4c5064221e1210457a50e330ced648265
Adelso Steve Araya Solórzano <stevearaya@Adelsos-MacBook-Pro.local>
2024-10-09 21:54:04 -0600

    SignUp page added

---
+++ cypress/e2e/pages/signup/signup.methods.js
@@ -0,0 +1,21 @@
+import { SignUpElements } from "./signup.elements";
+
+export class SignUpMethods {
+    static insertUserName(username) {
+        SignUpElements.textboxes.username.invoke('val', username);
+    }
+
+    static insertPassword(password) {
+        SignUpElements.textboxes.password.invoke('val', password);
+    }
+
+    static clickOnSignUpButton() {
+        SignUpElements.buttons.signup.click();
+    }
+
+    static signup(username, password) {
+        this.insertUserName(username);
+        this.insertPassword(password);
+        this.clickOnSignUpButton();
+    }
+}
