cf8cc27f847f8224318e3609a538b54bbec74dec
Adelso Steve Araya Solórzano <stevearaya@Adelsos-MacBook-Pro.local>
2024-10-09 22:23:09 -0600

    Product details page added

---
+++ cypress/e2e/pages/product-details/product-details.methods.js
@@ -0,0 +1,7 @@
+import { ProductDetailsEmelemnts } from "./product-details.elements";
+
+export class ProductDetailsMethods {
+    static clickOnAddToCartButton() {
+        ProductDetailsEmelemnts.buttons.addToCart.click();
+    }
+}
