468760c90cdd64215c7248652073d59c38871661
Adelso Steve Araya Solórzano <stevearaya@Adelsos-MacBook-Pro.local>
2024-10-10 20:23:07 -0600

    Registro de usuario valido added

--- cypress/e2e/tests/registro.cy.js
+++ cypress/e2e/tests/registro.cy.js
@@ -1,5 +1,29 @@
-describe('Registro y Autenticación', () => {
+import { CommonPageData } from "../pages/common-page/common-page.data";
+import { CommonPageMethods } from "../pages/common-page/common-page.methods";
+import { SignUpMethods } from "../pages/signup/signup.methods";
+import { Logger } from "../util/logger";
+
+describe(CommonPageData.testSuites.registro, () => {
     it('Registro de usuario valido', () => {
-        
+        Logger.stepNumber(1);
+        Logger.step('Navegar a la página de inicio');
+        CommonPageMethods.navigateToDemoBlaze();
+
+        Logger.stepNumber(2);
+        Logger.step('Hacer clic en "Sign up" en la barra de navegación');
+        CommonPageMethods.clickOnSignUpOption();
+
+        Logger.stepNumber(3);
+        Logger.step('Completar todos los campos obligatorios con información válida');
+        SignUpMethods.insertUserName('steve');
+        SignUpMethods.insertPassword('12345');
+
+        Logger.stepNumber(4);
+        Logger.step('Hacer clic en "Sign up" para registrar el usuario');
+        SignUpMethods.clickOnSignUpButton();
+        Logger.verification('Verificar que se muestre el mensaje "Sign up successful"');
+        SignUpMethods.verifySignupSuccesfulMessageIsDisplayed();
+
+
     });
 })
